255afbdc66d1c72d9e8fcd4a9029289b3d7e615b
Pratikchhya Shrestha <pat@tilde.team>
2022-06-01 18:29:57 -0500

    make class sealed
    for https://sonarcloud.io/project/issues?issues=AYEghSZ6sWv2pSZp-_hx&open=AYEghSZ6sWv2pSZp-_hx&id=patstha_filetransfer

--- FileTransfer.Persistence/DataService.cs
+++ FileTransfer.Persistence/DataService.cs
@@ -2,7 +2,7 @@
 
 namespace FileTransfer.Persistence
 {
-    public class DataService: IDataService, System.IDisposable
+    public sealed class DataService: IDataService, System.IDisposable
     {
         private readonly ILogger<DataService> _log;
         private readonly string _connectionString;
